201104205351aa9db21966e16cd6e63dd0ccfce3
Andrey <joker-host@yandex.ru>
2020-12-27 20:36:18 +0300

    Запросы через прокси

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -7,7 +7,7 @@ const dateFrom = moment(dateNow - 7 * 24 * 3600 * 1000).format('YYYY-MM-DD'); //
 
 class NewsApi {
   getArticles(keyWord) { // Поиск по ключевым словам
-    return fetch(`https://newsapi.org/v2/everything?q=${keyWord || localStorage.getItem('Key word')}&from=${dateFrom}&to=${dateNow}&pageSize=100&language=ru&apiKey=a349bbe623754e8cafb460c57b8ce4f1`,
+    return fetch(`https://nomoreparties.co/news/v2/everything?q=${keyWord || localStorage.getItem('Key word')}&from=${dateFrom}&to=${dateNow}&pageSize=100&language=ru&apiKey=a349bbe623754e8cafb460c57b8ce4f1`,
       {
         method: 'GET',
         headers: {
@@ -18,7 +18,7 @@ class NewsApi {
   }
 
   getTopArticles() { // Актуальные новости при загрузке страницы
-    return fetch('https://newsapi.org/v2/top-headlines?country=ru&language=ru&apiKey=a349bbe623754e8cafb460c57b8ce4f1',
+    return fetch('https://nomoreparties.co/news/v2/top-headlines?country=ru&language=ru&apiKey=a349bbe623754e8cafb460c57b8ce4f1',
       {
         method: 'GET',
         headers: {
